c48ad38e3d4c34148ad2d45ae5a33545e72d766d
nkyungm <skarudals27@naver.com>
2023-03-26 16:06:33 +0900

    Memo class 생성

---
+++ NgMemo/Model/Model.swift
@@ -0,0 +1,22 @@
+//
+//  Model.swift
+//  NgMemo
+//
+//  Created by 남경민 on 2023/03/26.
+//
+
+import Foundation
+class Memo{
+    var content: String
+    var insertDate : Date
+    
+    init(content: String){
+        self.content=content
+        insertDate=Date();
+    }
+    
+    static var dummyMemoList=[
+        Memo(content: "Lorem Ipsum"),
+        Memo(content: "Subscribe")
+    ]
+}
